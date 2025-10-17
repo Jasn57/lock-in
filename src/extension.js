cac90fd0d41fbfa5cbe2f4d75f2fc8cf9ec3b4cd
Jason <jasn74939@gmail.com>
2025-10-17 16:59:48 -0400

    write basic plugin logic

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,18 @@
-const vscode = require("vscode");
+const vscode = require('vscode');
 
-function activate("context") {
-  console.log("Lock In is now running");
+function activate(context) {
+    console.log('Extension "Hello World" is now active!');
+
+    let disposable = vscode.commands.registerCommand('hello-world.sayHello', function () {
+        
+    });
+
+    context.subscriptions.push(disposable);
 }
 
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
